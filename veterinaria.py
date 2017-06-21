c14dad2f062c53d3f2b1163dbc286592737982fd
Jairo Gonzalez <jairo.glez@hotmail.com>
2017-06-20 21:04:15 -0500

    codigo convertido a clase

--- veterinaria.py
+++ veterinaria.py
@@ -1,19 +1,20 @@
 import sqlite3
 
-db = sqlite3.connect("veterinaria.db")
-c = db.cursor()
+class Veterinaria:
+    db = sqlite3.connect("veterinaria.db")
+    c = db.cursor()
 
-def mostrarDueños():
-    f = c.execute("SELECT * FROM dueño")
-    f = f.fetchall()
+    def mostrarDueños(self):
+        f = self.c.execute("SELECT * FROM dueño")
+        f = f.fetchall()
+        lista = []
 
-    for fila in f:
-        print(fila)
+        for fila in f:
+            lista.append(fila)
 
-def insertarDueño(datos):
-    c.execute("INSERT INTO dueño (eMail,nombre,apellido1,apellido2,colonia,domicilio,telefono,contraseña,foto) VALUES (?,?,?,?,?,?,?,?,?)",
-    (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],datos[6],datos[7],datos[8]))
-    db.commit()
+        return lista
 
-insertarDueño(["juan@gmail.com","juan","lopez","duran","centro","roble #8","3378995423","passwooord","img78.jpg"])
-mostrarDueños()
+    def insertarDueño(self,datos):
+        self.c.execute("INSERT INTO dueño (eMail,nombre,apellido1,apellido2,colonia,domicilio,telefono,contraseña,foto) VALUES (?,?,?,?,?,?,?,?,?)",
+        (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],datos[6],datos[7],datos[8]))
+        self.db.commit()
