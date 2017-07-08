ffc37da216f9e0e605482f90459c994a51b1c01a
Jairo Gonzalez <jairo.glez@hotmail.com>
2017-07-07 20:57:01 -0500

    Metodo buscar implementado con menu

--- menuVet.py
+++ menuVet.py
@@ -6,6 +6,7 @@ class Menu:
         while True:
             print("1) Mostrar dueños")
             print("2) Agregar dueño")
+            print("3) buscar")
             print("0) Salir")
             op = input()
 
@@ -13,13 +14,15 @@ class Menu:
                 self.mostrar()
             elif op == "2":
                 self.capturar()
+            elif op == "3":
+                self.buscar()
             elif op == "0":
                 break
 
     def mostrar(self):
         usuarios = self.vet.mostrarDueños()
         for u in usuarios:
-            print("{0:25} {1:10} {2:15} {3:15} {4:10} {5:15} {6:10} {7:15} {8:10}z".format(u[0],u[1],u[2],u[3],u[4],u[5],u[6],u[7],u[8]))
+            print("{0:25} {1:10} {2:15} {3:15} {4:10} {5:15} {6:10} {7:15} {8:10}".format(u[0],u[1],u[2],u[3],u[4],u[5],u[6],u[7],u[8]))
 
     def capturar(self):
         correo = input("Correo: ")
@@ -33,3 +36,10 @@ class Menu:
         foto = input("Nombre de la foto: ")
 
         self.vet.insertarDueño([correo,nombre,apellido1,apellido2,colonia,domicilio,telefono,contraseña,foto])
+
+    def buscar(self):
+        palabra = input("Correo: ")
+        lista = self.vet.buscar(palabra)
+
+        for d in lista:
+            print("{0:25} {1:10} {2:15} {3:15} {4:10} {5:15} {6:10} {7:15} {8:10}".format(d["correo"],d["nombre"],d["apellido1"],d["apellido2"],d["colonia"],d["domicilio"],d["telefono"],d["password"],d["foto"]))
--- veterinaria.py
+++ veterinaria.py
@@ -18,3 +18,13 @@ class Veterinaria:
         self.c.execute("INSERT INTO dueño (eMail,nombre,apellido1,apellido2,colonia,domicilio,telefono,contraseña,foto) VALUES (?,?,?,?,?,?,?,?,?)",
         (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],datos[6],datos[7],datos[8]))
         self.db.commit()
+
+    def buscar(self,palabra):
+        self.c.execute("SELECT * FROM dueño WHERE eMail LIKE ?",("%"+palabra+"%",))
+        lista = []
+
+        for d in self.c:
+            dueño = {"correo":d[0],"nombre":d[1],"apellido1":d[2],"apellido2":d[3],"colonia":d[4],"domicilio":d[5],"telefono":d[6],"password":d[7],"foto":d[8]}
+            lista.append(dueño)
+
+        return lista
